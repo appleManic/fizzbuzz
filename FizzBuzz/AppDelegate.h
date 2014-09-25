7ace22bb6f78312ac817034bf9fa841fa33438c9
appleManic <pawanselokar@gmail.com>
2014-09-25 10:26:42 -0400

    Initial Commit

---
+++ FizzBuzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FizzBuzz
+//
+//  Created by Pawan selokar on 9/25/14.
+//  Copyright (c) 2014 NeRdS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FizzBuzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FizzBuzz
+//
+//  Created by Pawan selokar on 9/25/14.
+//  Copyright (c) 2014 NeRdS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
