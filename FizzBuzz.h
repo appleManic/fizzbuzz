175425af81aa149198ba3685798d95a01b3d925d
appleManic <pawanselokar@gmail.com>
2015-01-08 12:23:37 -0500

    first commit

---
+++ FizzBuzz.h
@@ -0,0 +1,13 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Pawan selokar on 9/25/14.
+//  Copyright (c) 2014 NeRdS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FizzBuzz : NSObject
+
+@end
---
+++ FizzBuzz.m
@@ -0,0 +1,13 @@
+//
+//  FizzBuzz.m
+//  FizzBuzz
+//
+//  Created by Pawan selokar on 9/25/14.
+//  Copyright (c) 2014 NeRdS. All rights reserved.
+//
+
+#import "FizzBuzz.h"
+
+@implementation FizzBuzz
+
+@end
